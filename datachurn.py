295399961ec03935e2497ebce32eeb372c50d330
Sakshi Singh <sakshi.singh@cohesity.com>
2025-04-11 12:49:49 +0530

    pulled latest

--- datachurn.py
+++ datachurn.py
@@ -9,7 +9,7 @@ from updater import pump_updates
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description='A program to populate a db in oracle',
-        usage='python3 pumper.py --host 10.14.69.121 --db_name prodsb21\
+        usage='python3 pumper.py --host 10.14.69.122 --db_name prodsb21\
                     --user sys --password cohesity --total_size 1G \
                     --datafile_size 200M --batch_size 200000 ')
     parser._action_groups.pop()
