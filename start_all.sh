82e406a296be5102953529e82ef4856004a63b41
Subha Bera <subha.bera@cohesity.com>
2025-08-05 16:29:12 +0530

    start script

--- start_all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run each script in background
-/root/oracle_data_pumper/venv/bin/python /root/oracle_data_pumper/pumper_startup.py --clusterip 10.14.7.1 &
-/root/oracle_data_pumper/venv/bin/python /root/oracle_data_pumper/pumper_startup.py --clusterip 10.2.197.147 &
-/root/oracle_data_pumper/venv/bin/python /root/oracle_data_pumper/pumper_startup.py --clusterip 10.131.32.2 &
-
-wait  # wait for all background jobs to finish (optional)
